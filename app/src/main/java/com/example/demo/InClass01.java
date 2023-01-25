4fbe8e740b5b7e11a6be916427275976969727ad
megananhdaonguyen <nguyen.meg@northeastern.edu>
2023-01-25 16:43:40 -0500

    what's cookin good lookin

--- app/src/main/java/com/example/demo/InClass01.java
+++ app/src/main/java/com/example/demo/InClass01.java
@@ -5,7 +5,6 @@ package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,16 +13,67 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class InClass01 extends AppCompatActivity {
-    private Button calculate;
-    private EditText weight_input;
+    private Button button_CalcBMI;
+    private EditText weight_Input;
+    private EditText feet_Input;
+    private EditText inches_Input;
+    private TextView text_Result;
+    private TextView condition_Output;
+
+    public static String TAG = "In Class";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EditText edit = (EditText) findViewById(R.id.weight_input);
-        //TextView tview = (TextView) findViewById(R.id.textview1);
-        Editable result = edit.getText();
-        //tview.setText(result);
+        setContentView(R.layout.calculate_bmi);
+        setTitle("Calculate BMI");
+
+        text_Result = findViewById(R.id.text_result);
+        condition_Output = findViewById(R.id.condition_output);
+
+        weight_Input = findViewById(R.id.weight_input);
+        feet_Input = findViewById(R.id.feet_input);
+        inches_Input = findViewById(R.id.inches_input);
+
+        button_CalcBMI = findViewById(R.id.button_calcBMI);
+        button_CalcBMI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float weight = 0;
+                float feet = 0;
+                float inches = 0;
+
+                String valueWeight = String.valueOf(weight_Input.getText());
+                String valueFeet = String.valueOf(feet_Input.getText());
+                String valueInches = String.valueOf(inches_Input.getText());
+
+                if (valueWeight.isEmpty() || valueFeet.isEmpty() || valueInches.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Text Box Empty",Toast.LENGTH_LONG).show();
+                } else {
+                    weight = Float.parseFloat(valueWeight);
+                    feet = Float.parseFloat(valueFeet);
+                    inches = Float.parseFloat(valueInches);
+                }
+
+                float total_inches = (feet * 12) + inches;
+
+                float bmi = (weight / (total_inches * total_inches)) * 703;
+                String condition = "";
+
+                if (bmi < 18.5) {
+                    condition = "Underweight";
+                } else if (bmi <= 24.9) {
+                    condition = "Normal weight";
+                } else if (bmi >= 25 && bmi <= 29.9) {
+                    condition = "Overweight";
+                } else {
+                    condition = "Obese";
+                }
+
+                text_Result.setText(String.format("Your BMI: %s", bmi));
+                condition_Output.setText(String.format("%s", condition));
+            }
+        });
 
     }
 }
