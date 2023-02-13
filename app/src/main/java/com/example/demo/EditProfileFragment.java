d7fc12cc333589595553ec5beae14c400a3a6920
megananhdaonguyen <nguyen.meg@northeastern.edu>
2023-02-13 15:59:17 -0500

    Committing InClass 03.

--- app/src/main/java/com/example/demo/EditProfileFragment.java
+++ app/src/main/java/com/example/demo/EditProfileFragment.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
